9955a0d9451cdd58bac8350de3098f13f94d0a72
Timur <156953282+TimurBased@users.noreply.github.com>
2024-08-28 17:06:14 +0300

    init

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,11 @@ import Button from '../components/Button'
 import { useTelegram } from '../hooks/useTelegram'
 
 const Header = () => {
-	const { user, onClose } = useTelegram()
+	const { tg, onClose } = useTelegram()
 	return (
 		<div className={'header'}>
 			<Button onClick={onClose}>Закрыть</Button>
-			<span className={'username'}>{user?.username}</span>
+			<span className={'username'}>{tg.initDataUnsafe?.user?.id}</span>
 		</div>
 	)
 }
